da0e1efb84db2894ac868f79486bfaf45476dcba
Alejo20032023 <jmorales.ipedep@gmail.com>
2024-02-23 19:03:18 -0600

    Se agrego el proyecto

---
+++ leerTeclado/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leer_Teclado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Suma de dos números");
+            Console.WriteLine("Ingresa el priumer numero: ");
+            //Los datos se leen como cadena 
+            string dato = Console.ReadLine();
+            //Convertir a número con int.Parse
+            int n1 = int.Parse(dato);
+
+            Console.WriteLine("Ingresa el segundo numero: ");
+            dato = Console.ReadLine();
+            int n2 = int.Parse(dato);
+            int suma = n1 + n2;
+            Console.WriteLine("El Resultado es:  {0}", suma);
+            Console.ReadLine();
+
+        }
+    }
+}
